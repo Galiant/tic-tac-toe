30a52a6ed7a0f57e4e365064b6d3f41621e34f6e
Antonijo <antonijodev@gmail.com>
2019-05-16 16:10:21 +0100

    O is also shows on the game board

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsnext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsnext ? 'X' : 'O';
+    this.setState({ squares: squares, xIsnext: !this.state.xIsnext });
   }
 
   renderSquare(i) {
